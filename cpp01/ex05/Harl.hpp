84fafca40021a702d0aff19e8bca0a7dbd94cae3
Mouad Labrirhil <mlabrirh@e1r9p3.1337.ma>
2025-11-28 15:36:08 +0100

    cpp01/ex05

---
+++ cpp01/ex05/Harl.hpp
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Harl.hpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mlabrirh <mlabrirh@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/11/28 15:26:59 by mlabrirh          #+#    #+#             */
+/*   Updated: 2025/11/28 15:34:22 by mlabrirh         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef HARL_HPP
+#define HARL_HPP
+#include <iostream>
+#include <string>
+
+class Harl
+{  
+    public:
+        Harl();
+        ~Harl();
+        void    complain(std::string level);
+    private:
+        void    debug( void );
+        void    info( void );
+        void    warning( void );
+        void    error( void );
+};
+
+#endif
---
+++ cpp01/ex05/main.cpp
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mlabrirh <mlabrirh@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/11/28 15:26:42 by mlabrirh          #+#    #+#             */
+/*   Updated: 2025/11/28 15:33:02 by mlabrirh         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Harl.hpp"
+
+int main()
+{
+    Harl harl;
+    harl.complain("DEBUG");
+    harl.complain("INFO");
+    harl.complain("WARNING");
+    harl.complain("ERROR");
+    harl.complain("UNKNOWN");
+    harl.complain("alarm");
+    return 0;
+}
